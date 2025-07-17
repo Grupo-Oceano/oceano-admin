3c7358335f2928e44ad8a4c2fe9d597d02f83465
NecroPrism4 <alexhid14@gmail.com>
2025-07-17 09:54:31 -0700

    fix: add default values for API_URL and NODE_ENV in environment.ts; update build.types script for prettier output

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 import { isBrowser } from "@builder.io/qwik";
 
-const { API_URL, NODE_ENV } = isBrowser ? {} : process?.env;
+const { API_URL = "", NODE_ENV = "" } = isBrowser ? {} : process?.env || {};
 
 export const environment = { API_URL, NODE_ENV };
